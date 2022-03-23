3b155bb24c53276472ffdd6b6da744296cd06611
Arthur <arthurarp@hotmail.com>
2022-03-23 20:48:48 -0300

    Adicionando primeira versao dos arquivos ddl e dml
    Co-authored-by: Joao Matheus <matheusr.unb@gmail.com>
    Co-authored-by: Marcelo Magalhães <magalhaesmarcelo@outlook.com.br>
    Co-authored-by: Joao Gabriel A <jg.antunes.medeiros@gmail.com>
    Co-authored-by: João Rossi <bielrossiborba@gmail.com>

---
+++ mod_4/ddl-v0.1.sql
@@ -0,0 +1,27 @@
+CREATE TABLE Personagem (
+  Nome VARCHAR(100) NOT NULL,
+  Tipo VARCHAR(20) NOT NULL,
+
+  CONSTRAINT personagem_pk PRIMARY KEY (Nome),
+  CONSTRAINT personagem_ck_tipo CHECK(Tipo IN ('viking', 'monstro'))
+);
+
+
+CREATE TABLE Viking (
+  Nome VARCHAR(100) NOT NULL,
+  Experiencia INTEGER NOT NULL,
+  Mochila INTEGER NOT NULL,
+  MaoEsquerda INTEGER,
+  MaoDireita INTEGER,
+  MaosOcupadas BOOLEAN NOT NULL,
+  Quadrado VARCHAR(10) NOT NULL,
+  Nivel INTEGER NOT NULL,
+
+  CONSTRAINT viking_pk PRIMARY KEY (Nome),
+  CONSTRAINT viking_mochila_fk FOREIGN KEY (Mochila) REFERENCES Mochila (Numero) ON DELETE CASCADE,
+  CONSTRAINT viking_mao_esquerda_fk FOREIGN KEY (MaoEsquerda) REFERENCES InstanciaItem (Id) ON DELETE CASCADE,
+  CONSTRAINT viking_mao_direita_fk FOREIGN KEY (MaoDireita) REFERENCES InstanciaItem (Id) ON DELETE CASCADE,
+  CONSTRAINT viking_quadrado_fk FOREIGN KEY (Quadrado) REFERENCES Quadrado (Coordenadas) ON DELETE CASCADE,
+  CONSTRAINT viking_nivel_fk FOREIGN KEY (Nivel) REFERENCES Nivel (Valor) ON DELETE CASCADE,
+);
+
---
+++ mod_4/dml-v0.1.sql
@@ -0,0 +1,4 @@
+INSERT INTO Personagem VALUES('marcelo', 'viking');
+INSERT INTO Personagem VALUES('arthur', 'viking');
+INSERT INTO Personagem VALUES('joao', 'viking');
+INSERT INTO Personagem VALUES('joao matheus', 'viking');
