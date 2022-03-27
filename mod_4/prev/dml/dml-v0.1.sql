d28b9188f7ef7ec363b4faecd25e68abeeafc66b
Arthur <arthurarp@hotmail.com>
2022-03-27 16:13:30 -0300

    apagando versoes desnecessarias
    Signed-off-by: Arthur <arthurarp@hotmail.com>

--- mod_4/prev/dml/dml-v0.1.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO Personagem VALUES('marcelo', 'viking');
-INSERT INTO Personagem VALUES('arthur', 'viking');
-INSERT INTO Personagem VALUES('joao', 'viking');
-INSERT INTO Personagem VALUES('joao matheus', 'viking');
