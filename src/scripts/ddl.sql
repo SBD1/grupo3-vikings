4ddcfd2552d1b8659fffcce7f608348d9b1ecd53
Arthur <arthurarp@hotmail.com>
2022-04-10 18:50:24 -0300

    Adicionando os arquivos DDL e DML à pasta source, com as adições das funções gatilho e melhorias em relação a entrega 4
    Signed-off-by: Arthur <arthurarp@hotmail.com>

---
+++ src/scripts/ddl.sql
@@ -0,0 +1,405 @@
+BEGIN TRANSACTION;
+
+CREATE TABLE Mapa (
+    ID INTEGER NOT NULL,
+
+    CONSTRAINT mapa_pk PRIMARY KEY (ID)
+);
+
+CREATE TABLE Area (
+    IdArea INTEGER NOT NULL,
+    Coordenadas VARCHAR(9) NOT NULL,
+    Tipo VARCHAR(5) NOT NULL DEFAULT 'terra',
+    Tamanho INTEGER NOT NULL,
+    IdMapa INTEGER NOT NULL,
+
+    CONSTRAINT area_pk PRIMARY KEY (IdArea),
+    CONSTRAINT area_id_mapa_fk FOREIGN KEY (IdMapa) REFERENCES Mapa (ID) ON DELETE CASCADE,
+
+    CONSTRAINT area_ck_tipo CHECK(Tipo IN ('terra', 'mar')),
+    CONSTRAINT area_ck_tamanho CHECK(Tamanho > 0)
+);
+
+CREATE TABLE Quadrado (
+    Coordenadas VARCHAR(9) NOT NULL,
+    Area INTEGER NOT NULL,
+    Descricao VARCHAR(200) NOT NULL,
+
+    CONSTRAINT quadrado_pk PRIMARY KEY (Coordenadas),
+    CONSTRAINT quadrado_area_fk FOREIGN KEY (Area) REFERENCES Area (IdArea) ON DELETE CASCADE
+);
+
+CREATE DOMAIN PERSONAGEM_TYPE
+  AS CHAR(7)
+  CHECK (VALUE IN ('viking', 'monstro'));
+
+CREATE TABLE Personagem (
+  Nome VARCHAR(100) NOT NULL,
+  Tipo PERSONAGEM_TYPE NOT NULL,
+
+  CONSTRAINT personagem_pk PRIMARY KEY (Nome)
+);
+
+CREATE TABLE Nivel (
+    Valor INTEGER NOT NULL,
+    Pontos_ao_Subir INTEGER NOT NULL,
+    Experiencia_para_Subir_de_Nivel INTEGER NOT NULL,
+
+    CONSTRAINT nivel_pk PRIMARY KEY (Valor),
+    CONSTRAINT nivel_ck_pontos_ao_subir CHECK(Pontos_ao_Subir > 0),
+    CONSTRAINT nivel_ck_experiencia_para_subir_de_nivel CHECK(Experiencia_para_Subir_de_Nivel > 0)
+);
+
+CREATE TABLE Tipo_Mochila (
+  Nome VARCHAR(20) NOT NULL,
+  Capacidade INTEGER NOT NULL,
+
+  CONSTRAINT tipo_mochila_pk PRIMARY KEY (Nome),
+  CONSTRAINT tipo_mochila_uq UNIQUE (Capacidade),
+  CONSTRAINT tipo_mochila_ck_capacidade CHECK(Capacidade > 0)
+);
+
+CREATE TABLE Mochila (
+  Numero INTEGER NOT NULL,
+  Tipo VARCHAR(20) NOT NULL,
+  VolumeOcupado INTEGER NOT NULL DEFAULT 0,
+
+  CONSTRAINT mochila_pk PRIMARY KEY (Numero),
+  CONSTRAINT mochila_tipo_fk FOREIGN KEY (Tipo) REFERENCES Tipo_Mochila (Nome) ON DELETE CASCADE,
+  CONSTRAINT tipo_mochila_ck_volume_ocupado CHECK(VolumeOcupado >= 0)
+);
+
+CREATE DOMAIN ITEM_TIPO
+  AS CHAR(8)
+  CHECK (VALUE IN ('arma', 'comida', 'pocao', 'armadura'));
+
+CREATE TABLE Especializacao_do_item(
+    Id INTEGER NOT NULL,
+    Tipo ITEM_TIPO NOT NULL,
+
+    CONSTRAINT especializacao_do_item_pk PRIMARY KEY(Id)
+);
+
+CREATE TABLE Instancia_item(
+    Id INTEGER NOT NULL,
+    Id_item INTEGER NOT NULL,
+    Quadrado VARCHAR(9),
+
+    CONSTRAINT instancia_item_pk PRIMARY KEY(Id),
+    CONSTRAINT id_item_instancia_item_fk FOREIGN KEY(Id_item) REFERENCES Especializacao_do_item(Id),
+    CONSTRAINT instancia_item_quadrado_fk FOREIGN KEY (Quadrado) REFERENCES Quadrado (Coordenadas) ON DELETE SET NULL
+);
+
+CREATE TABLE Item_Mochila (
+  Numero_Mochila INTEGER NOT NULL,
+  Id_Item INTEGER NOT NULL,
+
+  CONSTRAINT item_mochila_pk PRIMARY KEY (Numero_Mochila, Id_Item),
+  CONSTRAINT item_mochila_numero_mochila_fk FOREIGN KEY (Numero_Mochila) REFERENCES Mochila (Numero) ON DELETE CASCADE,
+  CONSTRAINT item_mochila_id_item_fk FOREIGN KEY (Id_Item) REFERENCES Instancia_item (Id) ON DELETE CASCADE
+);
+
+CREATE TABLE Viking (
+  Nome VARCHAR(100) NOT NULL,
+  
+  Experiencia INTEGER NOT NULL,
+  Nivel INTEGER NOT NULL,
+
+  Mochila INTEGER,
+  MaoEsquerda INTEGER,
+  MaoDireita INTEGER,
+  MaosOcupadas BOOLEAN NOT NULL,
+
+  Quadrado VARCHAR(9) NOT NULL,
+
+  -- Atributos_de_Luta
+  Ataque INTEGER NOT NULL,
+  Defesa INTEGER NOT NULL,
+  Roubo_de_Vida INTEGER NOT NULL,
+  Agilidade INTEGER NOT NULL,
+  Velocidade INTEGER NOT NULL,
+
+  -- Vida
+  Nivel_de_Vida INTEGER NOT NULL,
+  Vida_Restante INTEGER NOT NULL,
+
+  CONSTRAINT viking_pk PRIMARY KEY (Nome),
+  CONSTRAINT viking_nome_fk FOREIGN KEY (Nome) REFERENCES Personagem (Nome) ON DELETE CASCADE,
+  CONSTRAINT viking_nivel_fk FOREIGN KEY (Nivel) REFERENCES Nivel (Valor) ON DELETE CASCADE,
+  CONSTRAINT viking_mochila_fk FOREIGN KEY (Mochila) REFERENCES Mochila (Numero) ON DELETE SET NULL,
+  CONSTRAINT viking_mao_esquerda_fk FOREIGN KEY (MaoEsquerda) REFERENCES Instancia_item (Id) ON DELETE SET NULL,
+  CONSTRAINT viking_mao_direita_fk FOREIGN KEY (MaoDireita) REFERENCES Instancia_item (Id) ON DELETE SET NULL,
+  CONSTRAINT viking_quadrado_fk FOREIGN KEY (Quadrado) REFERENCES Quadrado (Coordenadas) ON DELETE CASCADE,
+
+  CONSTRAINT viking_ck_ataque CHECK(Ataque > 0),
+  CONSTRAINT viking_ck_defesa CHECK(Defesa > 0),
+  CONSTRAINT viking_ck_roubo_de_vida CHECK(Roubo_de_Vida > 0),
+  CONSTRAINT viking_ck_agilidade CHECK(Agilidade > 0),
+  CONSTRAINT viking_ck_velocidade CHECK(Velocidade > 0),
+  CONSTRAINT viking_ck_nivel_de_vida CHECK(Nivel_de_Vida >= 0),
+  CONSTRAINT viking_ck_vida_restante CHECK(Vida_Restante >= 0)
+);
+
+CREATE TABLE Barco (
+  Tipo VARCHAR(20) NOT NULL,
+  Capacidade INTEGER NOT NULL,
+  Velocidade INTEGER NOT NULL,
+  Ataque INTEGER NOT NULL,
+  Defesa INTEGER NOT NULL,
+
+  CONSTRAINT barco_pk PRIMARY KEY (Tipo),
+  CONSTRAINT barco_capacidade_ck CHECK(Capacidade > 0),
+  CONSTRAINT barco_velocidade_ck CHECK(Velocidade > 0),
+  CONSTRAINT barco_tipo_ck CHECK(Tipo IN ('knarr', 'langskip'))
+);
+
+CREATE TABLE InstanciaBarco (
+  IdBarco INTEGER NOT NULL,
+  Tipo VARCHAR(20) NOT NULL,
+  NomeViking VARCHAR(100),
+  Nome VARCHAR(100),
+  Integridade INTEGER NOT NULL,
+  Coordenadas VARCHAR(9) NOT NULL,
+
+  CONSTRAINT instancia_barco_pk PRIMARY KEY (IdBarco),
+  CONSTRAINT instancia_barco_tipo_fk FOREIGN KEY (Tipo) REFERENCES Barco (Tipo) ON DELETE CASCADE,
+  CONSTRAINT instancia_barco_viking_fk FOREIGN KEY (NomeViking) REFERENCES Viking (Nome) ON DELETE CASCADE
+);
+
+CREATE TABLE NPC (
+  IdNPC VARCHAR(100) NOT NULL,
+  Tribo VARCHAR(20) NOT NULL,
+  Genero SMALLINT NOT NULL,
+  Idade INTEGER NOT NULL,
+
+  CONSTRAINT npc_pk PRIMARY KEY (IdNPC)
+);
+
+CREATE TABLE InstanciaNPC (
+  Id INTEGER NOT NULL,
+  Tipo VARCHAR(100) NOT NULL,
+  EstaEmCidade BOOLEAN,
+  Quadrado VARCHAR(9) NOT NULL,
+
+  CONSTRAINT instancia_npc_pk PRIMARY KEY (Id),
+  CONSTRAINT instancia_npc_tipo_fk FOREIGN KEY (Tipo) REFERENCES NPC (IdNPC) ON DELETE CASCADE,
+  CONSTRAINT instancia_npc_quadrado_fk FOREIGN KEY (Quadrado) REFERENCES Quadrado (Coordenadas) ON DELETE CASCADE
+);
+
+CREATE TABLE Fala (
+  IdNPC VARCHAR(100) NOT NULL,
+  Texto VARCHAR(100) NOT NULL,
+
+  CONSTRAINT fala_pk PRIMARY KEY (IdNPC),
+  CONSTRAINT fala_npc_fk FOREIGN KEY (IdNPC) REFERENCES NPC (IdNPC) ON DELETE CASCADE,
+  CONSTRAINT fala_texto_ck CHECK(Texto != '')
+);
+
+CREATE TABLE TipoMonstro (
+  Nome VARCHAR(100) NOT NULL,
+  Experiencia INTEGER NOT NULL,
+  Descricao VARCHAR(1000) NOT NULL,
+  Bioma VARCHAR(20) NOT NULL,
+
+  CONSTRAINT tipo_monstro_pk PRIMARY KEY (Nome)
+);
+
+CREATE TABLE Monstro (
+  Nome VARCHAR(100) NOT NULL,
+  Quadrado VARCHAR(9) NOT NULL,
+  Tipo VARCHAR(100) NOT NULL,
+
+   -- Atributos_de_Luta
+  Ataque INTEGER NOT NULL,
+  Defesa INTEGER NOT NULL,
+  Roubo_de_Vida INTEGER NOT NULL,
+  Agilidade INTEGER NOT NULL,
+  Velocidade INTEGER NOT NULL,
+
+  -- Vida
+  Nivel_de_Vida INTEGER NOT NULL,
+  Vida_Restante INTEGER NOT NULL,
+
+  CONSTRAINT monstro_pk PRIMARY KEY (Nome),
+  CONSTRAINT monstro_nome_fk FOREIGN KEY (Nome) REFERENCES Personagem (Nome) ON DELETE CASCADE,
+  CONSTRAINT monstro_tipo_fk FOREIGN KEY (Tipo) REFERENCES TipoMonstro (Nome) ON DELETE CASCADE,
+  CONSTRAINT monstro_quadrado_fk FOREIGN KEY (Quadrado) REFERENCES Quadrado (Coordenadas) ON DELETE CASCADE,
+  CONSTRAINT monstro_ck_ataque CHECK(Ataque > 0),
+  CONSTRAINT monstro_ck_defesa CHECK(Defesa > 0),
+  CONSTRAINT monstro_ck_roubo_de_vida CHECK(Roubo_de_Vida > 0),
+  CONSTRAINT monstro_ck_agilidade CHECK(Agilidade > 0),
+  CONSTRAINT monstro_ck_velocidade CHECK(Velocidade > 0),
+  CONSTRAINT monstro_ck_nivel_de_vida CHECK(Nivel_de_Vida >= 0),
+  CONSTRAINT monstro_ck_vida_restante CHECK(Vida_Restante >= 0)
+);
+
+CREATE DOMAIN ITEM_RARIDADE
+  AS CHAR(8)
+  CHECK (VALUE IN ('comum', 'incomum', 'raro', 'epico', 'lendario', 'mitico'));
+
+CREATE TABLE Arma (
+    Id INTEGER NOT NULL,
+    Nome VARCHAR(100) NOT NULL,
+    Tipo ITEM_TIPO NOT NULL,
+    Descricao VARCHAR(300) NOT NULL,
+    Peso INTEGER NOT NULL,
+    Raridade ITEM_RARIDADE NOT NULL,
+    Consumivel BOOLEAN,
+    Valor_de_ataque INTEGER NOT NULL,
+    Valor_de_defesa INTEGER NOT NULL,
+    Valor_de_agilidade INTEGER NOT NULL,
+
+    CONSTRAINT arma_pk PRIMARY KEY(Id),
+    CONSTRAINT id_arma_fk FOREIGN KEY(Id) REFERENCES Especializacao_do_item(Id)
+);
+
+CREATE TABLE Comida (
+    Id INTEGER NOT NULL,
+    Nome VARCHAR(100) NOT NULL,
+    Tipo ITEM_TIPO NOT NULL,
+    Descricao VARCHAR(300) NOT NULL,
+    Peso INTEGER NOT NULL,
+    Raridade ITEM_RARIDADE NOT NULL,
+    Consumivel BOOLEAN,
+    Valor_de_cura INTEGER NOT NULL,
+
+    CONSTRAINT comida_pk PRIMARY KEY(Id),
+    CONSTRAINT id_comida_fk FOREIGN KEY(Id) REFERENCES Especializacao_do_item(Id)
+);
+
+CREATE TABLE Pocao (
+  Id INTEGER NOT NULL,
+  Nome VARCHAR(100) NOT NULL,
+  Tipo ITEM_TIPO NOT NULL,
+  Descricao VARCHAR(300) NOT NULL,
+  Peso INTEGER NOT NULL,
+  Raridade ITEM_RARIDADE NOT NULL,
+  Consumivel BOOLEAN,
+  Bonus INTEGER NOT NULL,
+  Duracao INTEGER NOT NULL,
+
+  CONSTRAINT pocao_pk PRIMARY KEY(Id),
+  CONSTRAINT id_pocao_fk FOREIGN KEY(Id) REFERENCES Especializacao_do_item(Id)
+);
+
+CREATE TABLE Armadura (
+  Id INTEGER NOT NULL,
+  Nome VARCHAR(100) NOT NULL,
+  Tipo ITEM_TIPO NOT NULL,
+  Descricao VARCHAR(300) NOT NULL,
+  Peso INTEGER NOT NULL,
+  Raridade ITEM_RARIDADE NOT NULL,
+  Consumivel BOOLEAN,
+  Valor_de_defesa INTEGER NOT NULL,
+  Valor_de_agilidade INTEGER NOT NULL,
+
+  CONSTRAINT armadura_pk PRIMARY KEY(Id),
+  CONSTRAINT id_armadura_fk FOREIGN KEY(Id) REFERENCES Especializacao_do_item(Id)
+);
+
+CREATE TABLE Entidade (
+    Nome VARCHAR(100) NOT NULL,
+    Descricao VARCHAR(300) NOT NULL,
+    
+    CONSTRAINT entidade_pk PRIMARY KEY(Nome)
+);
+
+CREATE TABLE Habilidade (
+    Nome VARCHAR(100) NOT NULL,
+    Multiplicador_agilidade INTEGER NOT NULL,
+    Multiplicador_ataque INTEGER NOT NULL,
+    Multiplicador_defesa INTEGER NOT NULL,
+    Multiplicador_roubo_de_vida INTEGER NOT NULL,
+    Descricao VARCHAR(300) NOT NULL,
+    Entidade VARCHAR(100) NOT NULL,
+    
+    CONSTRAINT habilidade_pk PRIMARY KEY(Nome),
+    CONSTRAINT entidade_habilidade_fk FOREIGN KEY(Entidade) REFERENCES Entidade(Nome)
+);
+
+CREATE TABLE Recebe (
+    Nome_habilidade VARCHAR(100) NOT NULL,
+    Nome_viking VARCHAR(100) NOT NULL,
+    Nome_entidade VARCHAR(100) NOT NULL,
+    
+    CONSTRAINT recebe_pk PRIMARY KEY(Nome_habilidade),
+    CONSTRAINT habilidade_nome_fk FOREIGN KEY(Nome_habilidade) REFERENCES Habilidade(Nome),
+    CONSTRAINT viking_nome_fk FOREIGN KEY(Nome_viking) REFERENCES Habilidade(Nome),
+    CONSTRAINT entidade_nome_fk FOREIGN KEY(Nome_entidade) REFERENCES Entidade(Nome)
+);
+
+CREATE TABLE Aesir (
+    Id VARCHAR(100) NOT NULL,
+    TipoEntidade VARCHAR(100) NOT NULL,
+    Bonus_ataque INTEGER NOT NULL,
+    Bonus_agilidade INTEGER NOT NULL,
+    
+    CONSTRAINT aesir_pk PRIMARY KEY(Id),
+    CONSTRAINT aesir_entidade_fk FOREIGN KEY(Id) REFERENCES Entidade(Nome)
+);
+
+CREATE TABLE Vanir (
+    Id VARCHAR(100) NOT NULL,
+    TipoEntidade VARCHAR(100) NOT NULL,
+    Bonus_defesa INTEGER NOT NULL,
+    Bonus_agilidade INTEGER NOT NULL,
+    
+    CONSTRAINT vanir_pk PRIMARY KEY(Id),
+    CONSTRAINT vanir_entidade_fk FOREIGN KEY(Id) REFERENCES Entidade(Nome)
+);
+
+CREATE TABLE Jotun (
+    Id VARCHAR(100) NOT NULL,
+    TipoEntidade VARCHAR(100) NOT NULL,
+    Bonus_ataque INTEGER NOT NULL,
+    Bonus_defesa INTEGER NOT NULL,
+    Bonus_agilidade INTEGER NOT NULL,
+    Maldicao INTEGER NOT NULL,
+    
+    CONSTRAINT jotun_pk PRIMARY KEY(Id),
+    CONSTRAINT jotun_entidade_fk FOREIGN KEY(Id) REFERENCES Entidade(Nome)
+);
+
+CREATE OR REPLACE FUNCTION 
+	atualizar_volume_ocupado_mochila()
+RETURNS TRIGGER AS $atualizar_volume_ocupado_mochila$
+
+BEGIN
+  IF (TG_OP = 'INSERT') THEN
+    UPDATE Mochila SET VolumeOcupado = VolumeOcupado + 1
+    WHERE Numero = NEW.Numero_Mochila;
+
+  ELSIF (TG_OP = 'DELETE') THEN
+    UPDATE Mochila SET VolumeOcupado = VolumeOcupado - 1
+    WHERE Numero = OLD.Numero_Mochila;
+  END IF;
+
+	RETURN NULL;
+END;
+$atualizar_volume_ocupado_mochila$ LANGUAGE plpgsql;
+
+CREATE TRIGGER AtualizarVolumeOcupado
+	AFTER INSERT OR DELETE ON Item_Mochila
+FOR EACH ROW EXECUTE PROCEDURE atualizar_volume_ocupado_mochila();
+
+CREATE OR REPLACE FUNCTION 
+	verificar_maos_ocupadas()
+RETURNS TRIGGER AS $verificar_maos_ocupadas$
+
+BEGIN
+  IF (NEW.MaoEsquerda IS NOT NULL AND NEW.MaoDireita IS NOT NULL)  THEN
+    NEW.MaosOcupadas := TRUE;
+  ELSE
+    NEW.MaosOcupadas := FALSE;
+  END IF;
+
+	RETURN NEW;
+END;
+$verificar_maos_ocupadas$ LANGUAGE plpgsql;
+
+CREATE TRIGGER PreencherVariavelMaosOcupadas
+	BEFORE INSERT OR UPDATE ON Viking
+FOR EACH ROW EXECUTE PROCEDURE verificar_maos_ocupadas();
+
+
+COMMIT;
---
+++ src/scripts/dml.sql
@@ -0,0 +1,108 @@
+BEGIN TRANSACTION;
+
+INSERT INTO Mapa VALUES(1);
+INSERT INTO Mapa VALUES(2);
+
+INSERT INTO Area VALUES(1, '0,0', 'terra', 2, 1);
+INSERT INTO Area VALUES(3, '0,0', 'mar', 2, 2);
+INSERT INTO Area (IdArea, Coordenadas, Tamanho, IdMapa) VALUES(4, '2,2', 4, 2);
+
+INSERT INTO Quadrado VALUES ('0,0', 1, 'Floresta');
+INSERT INTO Quadrado VALUES ('0,1', 1, 'Floresta');
+INSERT INTO Quadrado VALUES ('1,1', 1, 'Floresta');
+INSERT INTO Quadrado VALUES ('0,2', 1, 'Floresta');
+
+INSERT INTO Quadrado VALUES('2,2', 4, 'Floresta Negra');
+INSERT INTO Quadrado VALUES('3,3', 4, 'Vila velha');
+
+INSERT INTO Personagem VALUES('marcelo', 'viking');
+INSERT INTO Personagem VALUES('arthur', 'viking');
+INSERT INTO Personagem VALUES('sa', 'monstro');
+
+INSERT INTO Nivel VALUES(1, 100, 1000);
+INSERT INTO Nivel VALUES(2, 200, 2000);
+INSERT INTO Nivel VALUES(3, 300, 3000);
+
+INSERT INTO Tipo_Mochila VALUES('basica', 20);
+INSERT INTO Tipo_Mochila VALUES('normal', 35);
+INSERT INTO Tipo_Mochila VALUES('reforcada', 50);
+
+INSERT INTO Mochila VALUES(1, 'basica');
+INSERT INTO Mochila VALUES(2, 'normal');
+INSERT INTO Mochila VALUES(3, 'reforcada');
+INSERT INTO Mochila VALUES(4, 'basica');
+
+INSERT INTO Especializacao_do_item VALUES (1, 'arma');
+INSERT INTO Especializacao_do_item VALUES (2, 'arma');
+
+INSERT INTO Arma VALUES (1, 'Machado', 'arma', 'Machado corta cabeças', 20, 'comum', false, 30, 4, 3);
+INSERT INTO Arma VALUES (2, 'Espada', 'arma', 'Espada de prata', 6, 'epico', false, 70, 2, 90);
+
+INSERT INTO Instancia_item VALUES (1, 1, '1,1');
+INSERT INTO Instancia_item VALUES (2, 1, '1,1');
+
+INSERT INTO Instancia_item (Id, Id_item) VALUES (3, 1);
+INSERT INTO Instancia_item (Id, Id_item) VALUES (4, 1);
+INSERT INTO Instancia_item (Id, Id_item) VALUES (5, 2);
+
+INSERT INTO Instancia_item VALUES (6, 1, '0,1');
+
+INSERT INTO Viking (
+    Nome, Experiencia, Nivel, Quadrado, Ataque, Defesa, Roubo_de_Vida, Agilidade, Velocidade, Nivel_de_Vida, Vida_Restante, Mochila
+) VALUES ('arthur', 1500, 2, '2,2', 10, 11, 12, 13, 14, 15, 3, 1);
+
+INSERT INTO Viking VALUES ('marcelo', 150, 1, 2, 3, 4, true, '0,0', 3, 4, 5, 6, 7, 8, 9);
+
+INSERT INTO Personagem VALUES('joao', 'viking');
+
+INSERT INTO Viking (
+    Nome, Experiencia, Nivel, Mochila, MaoEsquerda, Quadrado, Ataque, Defesa, Roubo_de_Vida, Agilidade, Velocidade, Nivel_de_Vida, Vida_Restante
+) VALUES ('joao', 2500, 3, 3, 5, '2,2', 16, 17, 18, 19, 20, 21, 2);
+
+INSERT INTO Item_Mochila VALUES (1, 3);
+INSERT INTO Item_Mochila VALUES (1, 4);
+
+INSERT INTO Barco VALUES ('knarr', 15, 10, 100, 80);
+INSERT INTO Barco VALUES ('langskip', 15, 10, 100, 80);
+
+INSERT INTO InstanciaBarco VALUES (1, 'knarr', 'arthur', 'barco1', 100, '4,5');
+INSERT INTO InstanciaBarco VALUES (2, 'knarr', 'arthur', 'barco2', 100, '6,5');
+INSERT INTO InstanciaBarco VALUES (3, 'langskip', 'marcelo', 'barco3', 100, '3,5');
+
+INSERT INTO InstanciaBarco (
+    IdBarco, Tipo, Integridade, Coordenadas
+) VALUES (4, 'knarr', 100, '9,5');
+
+INSERT INTO Npc VALUES ('Ferreiro', 'tribo 01', 0, 51);
+INSERT INTO Npc VALUES ('Vendedor', 'tribo 01', 0, 51);
+
+INSERT INTO Quadrado VALUES('5,1', 3, 'Mar Gelado');
+INSERT INTO Quadrado VALUES('0,6', 4, 'Vila velha');
+
+INSERT INTO InstanciaNPC VALUES (1, 'Ferreiro', true, '5,1');
+
+INSERT INTO TipoMonstro VALUES ('Demonio', 1300, 'Assustador Demonio Viking', 'Pantano');
+
+INSERT INTO Personagem VALUES('Monstro 01', 'monstro');
+INSERT INTO Personagem VALUES('Monstro 02', 'monstro');
+INSERT INTO Monstro VALUES ('Monstro 01', '0,6', 'Demonio', 300, 250, 10, 50, 40, 100, 100);
+
+INSERT INTO Entidade VALUES('Thor', 'Deus nordico associado a forca, trovoes, relampagos e tempestades.');
+INSERT INTO Entidade VALUES('Loki', 'Deus nordico associado a trapaca, travessuras, fogo e tambem magia.');
+INSERT INTO Entidade VALUES('Freyja', 'Deusa-mae dos Vanir, associada a beleza,  fertilidade,  amor e seidhr, magia para visualizacao e alteracao do futuro.');
+INSERT INTO Entidade VALUES('Surtur', 'Lider dos gigantes de fogo e inimigo dos deuses, causador de destruicao extrema durante o ragnarok.');
+
+INSERT INTO Aesir VALUES('Thor', 'Aesir', 5, 1);
+INSERT INTO Aesir VALUES('Loki', 'Aesir', 1, 5);
+INSERT INTO Vanir VALUES('Thor', 'Vanir', 2, 4);
+INSERT INTO Jotun VALUES('Surtur', 'Jotun', 5, 5, 0, 5);
+
+INSERT INTO Habilidade VALUES('Arma de Trovao', 3, 5, 1, 1, 'Ao levantar sua arma e gritar o nome de Thor, o deus faz descer um raio para aumentar seu dano e agilidade', 'Thor');
+INSERT INTO Habilidade VALUES('Arma de Fogo e Sangue', 5, 2, 1, 5, 'Um pouco de sangue e uma  faisca sao o suficiente para Loki te ajudar a aumentar o dano e roubo de vida', 'Loki');
+INSERT INTO Habilidade VALUES('Visao do Futuro', 5, 1, 5, 1, 'Uma visao do futuro que ajuda na esquiva e defesa de ataques', 'Freyja');
+INSERT INTO Habilidade VALUES('Fogo primeval', 1, 1, 10, 1, 'Receba o poder do fogo primeval de Surtur, ganhando um enorme bonus de dano', 'Surtur');
+
+INSERT INTO NPC VALUES('Guia de Jornada', 'Tribo', 0, 62);
+INSERT INTO Fala VALUES('Guia de Jornada', 'Olá, eu sou o Guia de Jornada, seja bem vindo à vila.');
+
+COMMIT;
