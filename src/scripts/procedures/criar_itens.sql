9e944eeb91c5fcb507c2dc3f07c3b3c142e311ea
bielrossi15 <bielrossiborba@gmail.com>
2022-04-29 20:07:02 -0300

    Fixing itens values

--- src/scripts/procedures/criar_itens.sql
+++ src/scripts/procedures/criar_itens.sql
@@ -11,11 +11,11 @@ BEGIN
 
   INSERT INTO Arma 
     (Id, Nome, Descricao, Raridade, Peso, Valor_de_ataque, Valor_de_defesa, Valor_de_agilidade) 
-  VALUES (1, 'Machado', 'Machado corta cabeças', 'incomum', 7, 30, 4, 3);
-  INSERT INTO Arma VALUES (2, 'Espada', 'Espada de prata','comum', 6, 70, 2, 90);
-  INSERT INTO Arma VALUES (3, 'Faca', 'Faca simples', 'comum', 0.7, 70, 2, 90);
-  INSERT INTO Arma VALUES (4, 'Lança', 'Ponta de metal', 'comum', 5.5, 70, 2, 90);
-  INSERT INTO Arma VALUES (5, 'Porrete', 'Ideal para viagens de saque', 'comum', 7.5, 70, 2, 90);
+  VALUES (1, 'Machado', 'Machado corta cabeças', 'incomum', 7, 10, 4, 3);
+  INSERT INTO Arma VALUES (2, 'Espada', 'Espada de prata','comum', 6, 9, 2, 10);
+  INSERT INTO Arma VALUES (3, 'Faca', 'Faca simples', 'comum', 0.7, 9, 2, 10);
+  INSERT INTO Arma VALUES (4, 'Lança', 'Ponta de metal', 'comum', 5.5, 9, 2, 10);
+  INSERT INTO Arma VALUES (5, 'Porrete', 'Ideal para viagens de saque', 'comum', 7.5, 9, 2, 10);
 
   INSERT INTO Especializacao_do_item (Id, Tipo) VALUES (6, 'comida');
   INSERT INTO Especializacao_do_item VALUES (7, 'comida');
@@ -25,11 +25,11 @@ BEGIN
 
   INSERT INTO Comida
     (Id, Nome, Descricao, Raridade, Peso, Valor_de_cura)
-  VALUES (6, 'Peixe', 'Principal refeição', 'comum', 0.5, 40);
+  VALUES (6, 'Peixe', 'Principal refeição', 'comum', 0.5, 7);
   INSERT INTO Comida VALUES (7, 'Cereais', 'Aveia, Milho e Trigo', 'comum', 0.2, 15);
-  INSERT INTO Comida VALUES (8, 'Pão', 'Pão Nórdico de castanhas e sementes', 'comum', 0.6, 30);
+  INSERT INTO Comida VALUES (8, 'Pão', 'Pão Nórdico de castanhas e sementes', 'comum', 0.6, 6);
   INSERT INTO Comida VALUES (9, 'Fruta Seca', 'Fruta do bosque da Floresta negra', 'comum', 0.1, 5);
-  INSERT INTO Comida VALUES (10, 'Carne', 'A iguaria que te dará mais nutrientes', 'comum', 2, 85);
+  INSERT INTO Comida VALUES (10, 'Carne', 'A iguaria que te dará mais nutrientes', 'comum', 2, 9);
 
   INSERT INTO Especializacao_do_item (Id, Tipo) VALUES (11, 'pocao');
   INSERT INTO Especializacao_do_item VALUES (12, 'pocao');
@@ -37,17 +37,17 @@ BEGIN
 
   INSERT INTO Pocao
     (Id, Nome, Descricao, Raridade, Peso, Bonus, Duracao)
-  VALUES (11, 'Poção de Vida', 'Recupera parte sua vida instantaneamente', 'comum', 0.5, 70, 30);
-  INSERT INTO Pocao VALUES (12, 'Poção de Ataque', 'Aumenta o seu poder de ataque por alguns segundos', 'comum', 0.5, 40, 30);
-  INSERT INTO Pocao VALUES (13, 'Poção de Defesa', 'Aumenta o seu poder defensivo por alguns segundos', 'comum', 0.5, 40, 30);
+  VALUES (11, 'Poção de Vida', 'Recupera parte sua vida instantaneamente', 'comum', 0.5, 9, 7);
+  INSERT INTO Pocao VALUES (12, 'Poção de Ataque', 'Aumenta o seu poder de ataque por alguns segundos', 'comum', 0.5, 7, 7);
+  INSERT INTO Pocao VALUES (13, 'Poção de Defesa', 'Aumenta o seu poder defensivo por alguns segundos', 'comum', 0.5, 7, 7);
   
   INSERT INTO Especializacao_do_item (Id, Tipo) VALUES (14, 'armadura');
   INSERT INTO Especializacao_do_item VALUES (15, 'armadura');
 
   INSERT INTO Armadura
     (Id, Nome, Descricao, Raridade, Peso, Valor_de_defesa, Valor_de_agilidade)
-  VALUES (14, 'Armadura Básica', 'Te protege de ataques básicos', 'comum', 9.5, 30, 15);
-  INSERT INTO Armadura VALUES (15, 'Armadura Reforçada', 'Te protege dos principais ataques', 'comum', 12.5, 55, 30);
+  VALUES (14, 'Armadura Básica', 'Te protege de ataques básicos', 'comum', 9.5, 7, 15);
+  INSERT INTO Armadura VALUES (15, 'Armadura Reforçada', 'Te protege dos principais ataques', 'comum', 12.5, 7, 7);
 
 END;
 $$ LANGUAGE plpgsql;
